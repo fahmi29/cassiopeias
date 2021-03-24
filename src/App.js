37aa9d8542b3a0d7d180cab956b8adcfb3fb8108
Fahmi A <fdemoy29@gmail.com>
2021-03-24 15:06:26 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component, Fragment, lazy, Suspense } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+  } from "react-router-dom";
 import './App.css';
+const Landing = lazy(() => import('../src/component/Landing'))
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Suspense fallback={<></>}>
+            <Switch>
+              <Route exact path="/" component={Landing}></Route>  
+            </Switch>
+          </Suspense>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navigation from "../Navigation/Navigation";
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+                <Navigation />
+            </>
+        );
+    }
+}
+
+export default Home;
